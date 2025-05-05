b6843f42788c533ec49c83738cd1a602576fd638
Manuzinha <00001118610106sp@al.educacao.sp.gov.br>
2025-05-05 13:08:27 -0300

    primeiro commit

---
+++ mamãe.py
@@ -0,0 +1,25 @@
+import time
+
+mensagem = ["Feliz dia das Mães!!!",
+"Mãe, se hoje sou o que sou,",
+"se hoje sonho o que sonho ",
+"e se hoje tenho a força que tenho,",
+ "devo tudo isso a você.",
+
+ "Feliz Dia das Mães! ",
+ "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️"
+]
+for linha in mensagem :
+    print(linha)
+    time.sleep(1.5) #espere 1.5 segundos antes de mostrar a próxima linha 
+
+# criando um pequeno coração com caracteres 
+print()
+print("    **    **")
+print("  ****** ******")
+print(" **************")
+print("  ************")
+print("   **********")
+print("     ******")
+print("      ****")
+print("       **")
